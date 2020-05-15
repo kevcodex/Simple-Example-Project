01a31113d1841bed561cf6d0dd1f0ea43d0a8d10
Kevin Chen <kevchen2@gmail.com>
2020-05-14 17:52:08 -0700

    Initial Commit

---
+++ MoveMedicalTechnicalActivity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoveMedicalTechnicalActivity
+//
+//  Created by Kevin Chen on 5/14/20.
+//  Copyright © 2020 Kevin Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MoveMedicalTechnicalActivityTests/MoveMedicalTechnicalActivityTests.swift
@@ -0,0 +1,34 @@
+//
+//  MoveMedicalTechnicalActivityTests.swift
+//  MoveMedicalTechnicalActivityTests
+//
+//  Created by Kevin Chen on 5/14/20.
+//  Copyright © 2020 Kevin Chen. All rights reserved.
+//
+
+import XCTest
+@testable import MoveMedicalTechnicalActivity
+
+class MoveMedicalTechnicalActivityTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
